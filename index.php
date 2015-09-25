101ef87f2e48bef7233608e73dab278ae2cfbc54
Martinez <martinez@raidho.mx>
2015-09-25 13:02:24 -0500

    Se incluye una template base de Resultados de búsqueda
    Y se genera la estructura básica para los archivos de sass y css

--- index.php
+++ index.php
@@ -7,6 +7,47 @@ Index Template
 
 <?php include 'inc/header.php' ?>
 
-    <h1>Hello Brave New World</h1>
+<pre>Breadcrumbs</pre>
+
+<h1>Estás buscando "Vestidos" en Bridalog</h1>
+
+<ul>
+	<li><a href="#">
+			<img src="http://lorempixel.com/640/640/fashion" alt="">
+			<p>Categoría</p>
+			<p>Producto<span> por Proveedor</span></p>
+		</a></li>
+	<li><a href="#">
+			<img src="http://lorempixel.com/640/640/fashion" alt="">
+			<p>Categoría</p>
+			<p>Producto<span> por Proveedor</span></p>
+		</a></li>
+	<li><a href="#">
+			<img src="http://lorempixel.com/640/640/fashion" alt="">
+			<p>Categoría</p>
+			<p>Producto<span> por Proveedor</span></p>
+		</a></li>
+	<li><a href="#">
+			<img src="http://lorempixel.com/640/640/fashion" alt="">
+			<p>Categoría</p>
+			<p>Producto<span> por Proveedor</span></p>
+		</a></li>
+	<li><a href="#">
+			<img src="http://lorempixel.com/640/640/fashion" alt="">
+			<p>Categoría</p>
+			<p>Producto<span> por Proveedor</span></p>
+		</a></li>
+</ul>
+
+
+<pre>Pager</pre>
+
+<pre>Próxima búsqueda</pre>
+
+<pre>Categorías en el Catálogo</pre>
+
+<pre>Destacados en el Catálogo</pre>
+
+<pre>Más proveedores</pre>
 
 <?php include 'inc/footer.php' ?>
