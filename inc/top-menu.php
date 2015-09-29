77bdac790db5d559147664cca59166e4fae28cbe
Martinez <martinez@raidho.mx>
2015-09-29 18:13:46 -0500

    Estilos y markup de menu limpiados
    Se opta por otra forma de menus más sencilla usando toggle

--- inc/top-menu.php
+++ inc/top-menu.php
@@ -15,20 +15,7 @@ Top Navigation Template
 
 		<a class="toggleMenu" href="#">Menu</a>
 		<ul class="nav">
-			<li class="test">
-				<a href="#">Proveedores</a>
-				<ul>
-					<li>
-						<a href="#">Categorías</a>
-						<ul>
-							<li><a href="#">Iglesias</a></li>
-							<li><a href="#">Iluminación</a></li>
-							<li><a href="#">Invitaciones</a></li>
-							<li><a href="#">Joyas</a></li>
-						</ul>
-					</li>
-				</ul>
-			</li>
+			<li><a href="#">Proveedores</a>
 			<li><a href="#">Servicios</a></li>
 			<li><a href="#">Magazine</a></li>
 			<li><a href="#">Acerca</a></li>
