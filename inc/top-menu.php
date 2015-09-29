fc3b648f6bea8e0ae6aecb3a2d2b1b53ca2e91da
Martinez <martinez@raidho.mx>
2015-09-29 13:08:27 -0500

    Se agrega markup completo en el menú y estilos elementales

--- inc/top-menu.php
+++ inc/top-menu.php
@@ -5,26 +5,36 @@ Top Navigation Template
 */
 ?>
 
+<div id="top_menu">
+	<div class="wrap">
 
-	<a class="toggleMenu" href="#">Menu</a>
-	<ul class="nav">
-		<li class="test">
-			<a href="#">Proveedores</a>
-			<ul>
-				<li>
-					<a href="#">Categorías</a>
-					<ul>
-						<li><a href="#">Iglesias</a></li>
-						<li><a href="#">Iluminación</a></li>
-						<li><a href="#">Invitaciones</a></li>
-						<li><a href="#">Joyas</a></li>
-					</ul>
-				</li>
-			</ul>
-		</li>
-		<li><a href="#">Servicios</a></li>
-		<li><a href="#">Magazine</a></li>
-		<li><a href="#">Acerca</a></li>
-		<li><a href="#">Contacto</a></li>
-		<li><a href="#">Acceder</a></li>
-	</ul>
+		<div class="branding">
+			<a href="#">Bridalog</a>
+			<span>El <strong>catálogo</strong> para bodas hecho <strong>a la medida</strong></span>
+		</div>
+
+		<a class="toggleMenu" href="#">Menu</a>
+		<ul class="nav">
+			<li class="test">
+				<a href="#">Proveedores</a>
+				<ul>
+					<li>
+						<a href="#">Categorías</a>
+						<ul>
+							<li><a href="#">Iglesias</a></li>
+							<li><a href="#">Iluminación</a></li>
+							<li><a href="#">Invitaciones</a></li>
+							<li><a href="#">Joyas</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li><a href="#">Servicios</a></li>
+			<li><a href="#">Magazine</a></li>
+			<li><a href="#">Acerca</a></li>
+			<li><a href="#">Contacto</a></li>
+			<li><a href="#">Acceder</a></li>
+		</ul>
+
+	</div>
+</div>
