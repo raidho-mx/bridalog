2339a0965a50d05fb91e8e6f021db6cff364bba9
Martinez <martinez@raidho.mx>
2015-09-30 16:14:49 -0500

    Se terminan estilos para listado de más proveedores (index-list)

--- index.php
+++ index.php
@@ -66,87 +66,114 @@ Index Template
 <div class="wrap"> <!--Wrap open -->
 
 	<!-- Listado de categorías (íconos) -->
-	<div>
+	<div class="center_titles">
 		<h3 class="asap blue">Categorías en el Catálogo</h3>
-		<ul class="icon_grid basic_grid">
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
-			<li><a href="#"><img src="lib/img/icon-sample.jpg" alt=""><span href="" class="small_p">Categoría</span></a></li>
+		<ul class="menu_icon_grid asap basic_grid">
+			<li><a href="#"><img src="lib/img/icons/icon-catering.png" alt=""><span href="" class="small_p">Catering</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-iglesia.png" alt=""><span href="" class="small_p">Coordinación Iglesia</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-decoracion.png" alt=""><span href="" class="small_p">Decoración</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-flores.png" alt=""><span href="" class="small_p">Flores</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-foto.png" alt=""><span href="" class="small_p">Fotografía & Video</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-iglesia-lugar.png" alt=""><span href="" class="small_p">Iglesias</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-iluminacion.png" alt=""><span href="" class="small_p">Iluminación</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-invitaciones.png" alt=""><span href="" class="small_p">Invitaciones & Detalles</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-joyas.png" alt=""><span href="" class="small_p">Joyas</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-lugar.png" alt=""><span href="" class="small_p">Lugar</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-luna.png" alt=""><span href="" class="small_p">Lunas de Miel</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-menswear.png" alt=""><span href="" class="small_p">Menswear</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-mesaregalos.png" alt=""><span href="" class="small_p">Mesa de Regalos</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-mesapostres.png" alt=""><span href="" class="small_p">Mesa de Postres</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-premaritales.png" alt=""><span href="" class="small_p">Pláticas Pre- Maritales</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-mobiliario.png" alt=""><span href="" class="small_p">Renta de Mobiliario</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-belleza.png" alt=""><span href="" class="small_p">Salón & Salud</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-otros.png" alt=""><span href="" class="small_p">Otros</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-vestidos.png" alt=""><span href="" class="small_p">Vestidos & Accesorios</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-vinos.png" alt=""><span href="" class="small_p">Vinos & Licores</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-planner.png" alt=""><span href="" class="small_p">Wedding Planner</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/icon-zapatos.png" alt=""><span href="" class="small_p">Zapatos</span></a></li>
 		</ul>
 	</div>
 
 	<!-- Destacados en el Catálogo -->
-	<div>
+	<div class="center_titles">
 		<h3 class="asap blue">Destacados en el Catálogo</h3>
 		<ul class="basic_grid three_col_grid">
 			<li><a href="#">
 					<img src="http://lorempixel.com/640/640/city" alt="">
-					<p>Categoría</p>
-					<h4>Producto<span> por Proveedor</span></h4>
+					<p class="asap gray_medium">Categoría</p>
+					<h4>Proveedor</span></h4>
 				</a></li>
 			<li><a href="#">
 					<img src="http://lorempixel.com/640/640/city" alt="">
-					<p>Categoría</p>
-					<h4>Producto<span> por Proveedor</span></h4>
+					<p class="asap gray_medium">Categoría</p>
+					<h4>Proveedor</span></h4>
 				</a></li>
 			<li><a href="#">
 					<img src="http://lorempixel.com/640/640/city" alt="">
-					<p>Categoría</p>
-					<h4>Producto<span> por Proveedor</span></h4>
+					<p class="asap gray_medium">Categoría</p>
+					<h4>Proveedor</span></h4>
 				</a></li>
 		</ul>
 	</div>
 
 	<!-- Más proveedores -->
-	<div>
+	<div class="center_titles">
 		<h3 class="asap blue">Más proveedores</h3>
-		<ul class="basic_grid four_col_grid">
+		<ul class="basic_grid four_col_grid index_list">
 			<li>
-				<div><img src="http://lorempixel.com/20/20/cats" alt=""></div>
-				<div>
-					<p>Pasteles</p>
-					<a href="#">The Cake Studio</a>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
 				</div>
 			</li>
 			<li>
-				<div><img src="http://lorempixel.com/20/20/cats" alt=""></div>
-				<div>
-					<p>Pasteles</p>
-					<a href="#">The Cake Studio</a>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
 				</div>
 			</li>
 			<li>
-				<div><img src="http://lorempixel.com/20/20/cats" alt=""></div>
-				<div>
-					<p>Pasteles</p>
-					<a href="#">The Cake Studio</a>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
 				</div>
 			</li>
 			<li>
-				<div><img src="http://lorempixel.com/20/20/cats" alt=""></div>
-				<div>
-					<p>Pasteles</p>
-					<a href="#">The Cake Studio</a>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
 				</div>
 			</li>
 		</ul>
