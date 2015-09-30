1d7c043312e616e7c9b81ddc165d964374cc39d7
Martinez <martinez@raidho.mx>
2015-09-30 16:19:38 -0500

    Se incluye footer estable

--- inc/footer.php
+++ inc/footer.php
@@ -6,25 +6,26 @@ Footer Template
 ?>
 
 		<footer id="footer">
-		    <section class="wrap">
-		    	<div class="foot_signature">
-		    		<h1><a href="http://bridalog.com/">Bridalog</a></h1>
-		    		<p class="lora">Todos los derechos reservados, © Bridalog 2012</p>
-		    	</div>
-		    	<nav id="footer_nav">
-		    		<ul class="normal_foot_nav asap">
-		    			<!-- <li><a href="http://bridalog.com/">Proveedores</a></li> -->
-		    			<!-- <li><a href="http://bridalog.com/">Productos</a></li> -->
-		    			<li><a href="http://bridalog.com/magazine">Magazine</a></li>
-		    			<li><a href="http://bridalog.com/acerca-de">Acerca de</a></li>
-		    			<li><a href="http://bridalog.com/contacto">Contacto</a></li>
-		    		</ul>
-		    		<ul class="mini_foot_nav asap">
-		                <li><a href="http://bridalog.com/condiciones">Términos y Condiciones</a></li>
-		                <li><a href="http://bridalog.com/proveedor/entrar">Proveedor</a></li>
-		            </ul>
-		    	</nav>
-		    </section>
+			<div id="bottom_menu">
+				<div class="wrap">
+
+					<div class="branding">
+						<a class="habano regular_title" href="#">Bridalog</a>
+						<span class="small_p">Todos los derechos reservados</span>
+					</div>
+
+					<a class="toggleMenu" href="#">Menu</a>
+					<ul class="nav">
+						<li><a href="#">Proveedores</a>
+						<li><a href="#">Servicios</a></li>
+						<li><a href="#">Magazine</a></li>
+						<li><a href="#">Acerca</a></li>
+						<li><a href="#">Contacto</a></li>
+						<li><a href="#">Acceder</a></li>
+					</ul>
+
+				</div>
+			</div>
 		</footer>
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
