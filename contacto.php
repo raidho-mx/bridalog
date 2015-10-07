220c2ac5135e60f887f2b5a4b913ef98f7be95f1
Martinez <martinez@raidho.mx>
2015-10-07 17:30:56 -0500

    Sección Contacto al 90%
    Formato agregado para formas en general, y estilos para bloques de redes sociales

--- contacto.php
+++ contacto.php
@@ -16,21 +16,36 @@ Contact Template
 </div>
 
 <div class="wrap">
-	<div class="contact_form">
+	<div class="contact_form inner_section_pad centered_section">
+		<h4 class="asap">Cuéntanos, ¿qué podemos hacer por ti?</h4>
 		<form action="">
-			<input name="contacto_nombre" type="text" placeholder="Tu Nombre">
-			<input name="contacto_correo" type="text" placeholder="Tu Correo">
-			<input name="contacto_asunto" type="select" selected="Elige un Asunto">
-			<textarea name="contacto_mensaje" id="" cols="30" rows="10" placeholder="¿Cómo podemos ayudarte?"></textarea>
-			<input type="submit" name="submit" value="Enviar">
+		<div class="two_col_grid basic_grid group">
+
+			<div>
+				<input name="contacto_nombre" type="text" placeholder="Tu Nombre">
+				<input name="contacto_correo" type="text" placeholder="Tu Correo">
+				<select name="contacto_asunto">
+					<option value="default">Elige un asunto</option>
+					<option value="soporte">Requiero ayuda para usar su sitio</option>
+					<option value="proveedor">Me interesa ser proveedor</option>
+					<option value="eventos">Requiero información de sus eventos</option>
+					<option value="sugerencias">Tengo una sugerencia sobre el portal</option>
+				</select>
+			</div>
+			<div>
+				<textarea name="contacto_mensaje" id="" cols="30" rows="10" placeholder="¿Cómo podemos ayudarte?"></textarea>
+			</div>
+
+		</div>
+		<input type="submit" name="submit" value="Enviar">
 		</form>
-		<p>Campos marcados en rosa son obligatorios. Al dar click en ‘Enviar tus datos’ indicas que conoces nuestras <strong><a href="#">Políticas de Privacidad</a></strong>.</p>
+		<p class="disclaimer asap gray_medium">Campos marcados en rosa son obligatorios. Al dar click en ‘Enviar tus datos’ indicas que conoces nuestras <strong><a href="#">Políticas de Privacidad</a></strong>.</p>
 	</div>
 </div> <!--Wrap -->
 
-<div class="white_bg">
+<div class="white_bg full_width centered_section">
 	<div class="wrap">
-		<h3>Bridalog en Redes Sociales</h3>
+		<h3 class="asap">Bridalog en Redes Sociales</h3>
 		<ul class="insta_feed basic_grid four_col_grid">
 			<li><a href="#"><img src="http://lorempixel.com/640/640/city" alt=""></a></li>
 			<li><a href="#"><img src="http://lorempixel.com/640/640/city" alt=""></a></li>
@@ -38,16 +53,17 @@ Contact Template
 			<li><a href="#"><img src="http://lorempixel.com/640/640/city" alt=""></a></li>
 		</ul>
 		<ul class="share_btns">
-			<li><a href="#"><span>f</span> Bridalog</a></li>
-			<li><a href="#"><span>i</span> @bridalog</a></li>
-			<li><a href="#"><span>p</span> Bridalog</a></li>
+			<li>
+			<a class="btn_facebook" href="#"><span class="font_awesome"></span> Bridalog</a></li><li>
+			<a class="btn_instagram" href="#"><span class="font_awesome"></span> @bridalog</a></li><li>
+			<a class="btn_pinterest" href="#"><span class="font_awesome"></span> Bridalog</a></li>
 		</ul>
 	</div><!--Wrap -->
 </div>
 
-<div class="wrap">
-	<div class="centered_medium">
-		<p>Campos marcados en rosa son obligatorios. Al dar click en ‘Enviar tus datos’ indicas que conoces nuestras <strong><a href="#">Políticas de Privacidad</a></strong>.</p>
+<div class="wrap centered_section">
+	<div>
+		<p class="small_title">Durante los últimos 3 años, <strong>Bridalog</strong> ha crecido como una de las opciones más confiables para encontrar y dar a conocer servicios premium para bodas. <strong>Bridalog</strong> funciona gracias a su comunidad de novias participativas, así como su red selecta de proveedores líderes en sus áreas.</p>
 	</div>
 	<div class="squared_btn">
 		<a href="#">Conoce la historia detrás de Bridalog</a>
