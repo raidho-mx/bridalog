a4cc7910625e60b971b9f3d1964f83ca50d07e9a
Martinez <martinez@raidho.mx>
2015-10-08 16:57:08 -0500

    Template base Prov Individual agregado

--- inc/top-menu.php
+++ inc/top-menu.php
@@ -17,6 +17,7 @@ Top Navigation Template
 		<ul class="nav">
 			<li><a id="icon_grid_toggl" href="#">Categorías</a>
 			<li><a href="proveedores.php">Proveedores</a>
+			<li><a href="proveedor.php">S.-Prov.</a>
 			<li><a href="servicios.php">Servicios</a></li>
 			<li><a href="#">Magazine</a></li>
 			<li><a href="acerca.php">Acerca</a></li>
---
+++ proveedor.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Bridalog Website
+Contact Template
+*/
+?>
+
+<?php include 'inc/header.php' ?>
+
+<div class="image_cover" style="background-image: url('http://lorempixel.com/1400/640/city')">
+	<div class="wrap">
+		<div class="cover_text">
+			<h1 class="Habano">Bermellón</h1>
+		</div>
+	</div>
+</div>
+
+<div class="wrap">
+	<section class="prov_info">
+		<div class="prov_main_info">
+		</div>
+
+		<div class="prov_content_related">
+		</div>
+
+		<div class="prov_products">
+		</div>
+	</section>
+	<aside class="prov_sidebar"></aside>
+
+	<!-- Destacados en el Catálogo -->
+	<div class="center_titles">
+		<h3 class="asap blue">Destacados en el Catálogo</h3>
+		<ul class="basic_grid three_col_grid">
+			<li><a href="#">
+					<img src="http://lorempixel.com/640/640/city" alt="">
+					<p class="asap gray_medium">Categoría</p>
+					<h4>Proveedor</span></h4>
+				</a></li>
+			<li><a href="#">
+					<img src="http://lorempixel.com/640/640/city" alt="">
+					<p class="asap gray_medium">Categoría</p>
+					<h4>Proveedor</span></h4>
+				</a></li>
+			<li><a href="#">
+					<img src="http://lorempixel.com/640/640/city" alt="">
+					<p class="asap gray_medium">Categoría</p>
+					<h4>Proveedor</span></h4>
+				</a></li>
+		</ul>
+	</div>
+
+</div> <!--Wrap -->
+
+
+
+<?php include 'inc/footer.php' ?>
