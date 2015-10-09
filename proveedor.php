804c6329e3a702d2eb9f5302107458fb381f345f
Martinez <martinez@raidho.mx>
2015-10-09 18:25:38 -0500

    Sección Proveedor Individual 90%
    Falta agregar imágenes reales, cambiar ícono portada a blanco

--- proveedor.php
+++ proveedor.php
@@ -7,26 +7,209 @@ Contact Template
 
 <?php include 'inc/header.php' ?>
 
+<div class="prov_single">
+
 <div class="image_cover" style="background-image: url('http://lorempixel.com/1400/640/city')">
 	<div class="wrap">
 		<div class="cover_text">
-			<h1 class="Habano">Bermellón</h1>
+			<div>
+				<h1 class="Habano">Bermellón</h1>
+				<img src="lib/img/icons/icon-mesapostres.png" alt="">
+				<p>Mesa de Postres <br>
+				<span>– est. 2012 –</span></p>
+			</div>
 		</div>
 	</div>
 </div>
 
 <div class="wrap">
+	<div class="group">
 	<section class="prov_info">
-		<div class="prov_main_info">
+		<div class="prov_main_info group">
+			<div class="prov_main_info_profile">
+				<div>
+					<h4>¡Una delicia para tu paladar! Bermellón te ofrece una gran variedad de tamarindos y botanas para venir a disfrutar con tus amigas.</h4>
+					<p>Bermellón es una tienda de dulces picantes artesanales, queremos que conozcan quien esta detrás de estos dulces y mesas de postres tan extraordinarias. Ellas son Norma y Fernanda Ortiz y son quienes renovaron el concepto de poner dulces picantes y papitas de una boda "¡Quien se hubiera imaginado que comer papas, elotes y chilitos en una boda se iba a ver elegante!".</p>
+				</div>
+				<div>
+					<img src="http://lorempixel.com/640/640/city" alt="">
+					<p><strong>Bridalog Presenta</strong>: Bermellón, Dulces Picantes Artesanales.</p>
+				</div>
+			</div>
+			<div class="prov_main_contact asap">
+				<div>
+					<p><strong>Dirección</strong></p>
+					<ul>
+						<li>Centrito Valle - Río Grijalva NO 217 local 4 entre Orinoco y Amazonas, Col. Del Valle 66220 San Pedro Garza García, NL</li>
+						<li><a href="#">Abrir en Google Maps</a></li>
+					</ul>
+				</div>
+				<div>
+					<p><strong>Contacto</strong></p>
+					<ul>
+						<li>(81) 1935 7080</li>
+						<li><a href="#">hola@bermellon.mx</a></li>
+						<li><a href="#">www.bermellon.mx</a></li>
+					</ul>
+				</div>
+				<div>
+					<p><strong>Redes</strong></p>
+					<ul>
+						<li><a href="#"><span class="font_awesome"></span> Bermellón en Facebook</a></li>
+						<li><a href="#"><span class="font_awesome"></span> Bermellón en Twitter</a></li>
+					</ul>
+				</div>
+			</div>
+
 		</div>
 
-		<div class="prov_content_related">
+		<div class="prov_content_related group">
+			<div><img src="http://lorempixel.com/640/340/city" alt=""></div>
+			<div>
+				<h4 class="asap gray_medium">Desde el Magazine</h4>
+				<h3>Evento Bridalog en Bermellón</h3>
+			</div>
 		</div>
 
-		<div class="prov_products">
+		<div>
+			<h4 class="asap">Productos de Bermellón</h4>
+			<!-- Main search results -->
+			<ul class="prov_products_slim basic_grid">
+				<li><a href="#">
+						<img src="http://lorempixel.com/640/640/city" alt="">
+						<p>
+							<span class="asap gray_medium">Categoría</span>
+							<br>Producto
+						</p>
+					</a></li>
+				<li><a href="#">
+						<img src="http://lorempixel.com/640/640/city" alt="">
+						<p>
+							<span class="asap gray_medium">Categoría</span>
+							<br>Producto
+						</p>
+					</a></li>
+				<li><a href="#">
+						<img src="http://lorempixel.com/640/640/city" alt="">
+						<p>
+							<span class="asap gray_medium">Categoría</span>
+							<br>Producto
+						</p>
+					</a></li>
+				<li><a href="#">
+						<img src="http://lorempixel.com/640/640/city" alt="">
+						<p>
+							<span class="asap gray_medium">Categoría</span>
+							<br>Producto
+						</p>
+					</a></li>
+				<li><a href="#">
+						<img src="http://lorempixel.com/640/640/city" alt="">
+						<p>
+							<span class="asap gray_medium">Categoría</span>
+							<br>Producto
+						</p>
+					</a></li>
+				<li><a href="#">
+						<img src="http://lorempixel.com/640/640/city" alt="">
+						<p>
+							<span class="asap gray_medium">Categoría</span>
+							<br>Producto
+						</p>
+					</a></li>
+			</ul>
+
+			<!-- Pager -->
+			<div class="bottom_pager">
+				<ul>
+					<li><a href="#">1</a></li>
+					<li><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+				</ul>
+			</div>
 		</div>
 	</section>
-	<aside class="prov_sidebar"></aside>
+
+	<aside class="prov_sidebar">
+		<h4 class="asap">Proveedores Relacionados</h4>
+		<ul class="thumbs_list group">
+			<li>
+				<div><a href="#"><img src="http://lorempixel.com/100/100/city" alt=""></a></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><a href="#"><img src="http://lorempixel.com/100/100/city" alt=""></a></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><a href="#"><img src="http://lorempixel.com/100/100/city" alt=""></a></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><a href="#"><img src="http://lorempixel.com/100/100/city" alt=""></a></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><a href="#"><img src="http://lorempixel.com/100/100/city" alt=""></a></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+		</ul>
+
+		<h4 class="asap">Más proveedores</h4>
+		<ul class="index_list">
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+			<li>
+				<div><p class="asap"><a href="#">T</a></p></div>
+				<div class="small_p">
+					<p class="asap gray_medium"><a href="#">Pasteles</a></p>
+					<p><a href="#">The Cake Studio</a></p>
+				</div>
+			</li>
+		</ul>
+	</aside>
+</div><!-- Group ends (Clearfix) -->
 
 	<!-- Destacados en el Catálogo -->
 	<div class="center_titles">
@@ -52,6 +235,6 @@ Contact Template
 
 </div> <!--Wrap -->
 
-
+</div> <!-- prov_single -->
 
 <?php include 'inc/footer.php' ?>
