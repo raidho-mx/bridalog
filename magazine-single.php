d01147d08088e439937d920f94cdc6399be6d3ab
Martinez <martinez@raidho.mx>
2015-10-14 19:24:04 -0500

    Se termina el cuerpo principal de noticia en Single Magazine

--- magazine-single.php
+++ magazine-single.php
@@ -13,23 +13,44 @@ Contact Template
 
 <div class="wrap">
 	<div class="body_sidebar_combo group">
-	<section class="combo_body">
-		<div class="mag_article group">
-			<p>Food & Drink</p>
-			<h1> Smoothies Bonanza para Bodas Outdoor.</h1>
-		</div>
 
-		<div class="prov_content_related group">
-			<div><img src="http://lorempixel.com/640/340/city" alt=""></div>
-			<div>
-				<h4 class="asap gray_medium">Desde el Magazine</h4>
-				<h3>Evento Bridalog en Bermellón</h3>
+		<section class="mag_article combo_body">
+
+			<div class="mag_article_container asap white_bg">
+				<p>Food & Drink</p>
+				<h1> Smoothies Bonanza para Bodas Outdoor.</h1>
+				<ul class="share_btns">
+					<li><a href="#"><span class="font_awesome"></span></a></li><li>
+					<a class="btn_instagram" href="#"><span class="font_awesome"></span></a></li><li>
+					<a class="btn_pinterest" href="#"><span class="font_awesome"></span></a></li>
+				</ul>
+
+				<p class="small_title">A lot of people like smoothies. But few people make them at home. Because think of all the measuring, the effort, the mess.It doesn’t have to be that way. Pick a few of these recipes, hit the grocery store, then make “smoothie packs” for the whole week by placing measured-out ingredients for each smoothie in a plastic bag. When you need to be refreshed, pull a bag out of the freezer, dump its contents in a blender, and YAY.</p>
+				<p>The below recipes are perfect because they only have three ingredients each and they use frozen fruits and veggies so you can skip the ice. You can freeze your own fruits and veggies if you’re not happy with the frozen stuff from the grocery store. You can even freeze the almond milk or coconut milks in ice trays, if you really want to impress someone.</p>
+				<img src="http://lorempixel.com/640/340/city" alt="">
+				<h4>Strawberry Swing</h4>
+				<img src="http://lorempixel.com/640/340/city" alt="">
+				<p><strong>How to:</strong> Place 8 frozen strawberries, ½ cup plain kefir or pourable plain yogurt, and ½ cup shredded, unsweetened coconut into a blender. Blend until smooth. Serves 1.</p>
+				<p><strong>Extras:</strong> Spark it up with 2 tablespoons rolled oats, pinch ground cinnamon, 2 tablespoons flax meal, or protein powder.</p>
+
+				<ul class="share_btns">
+					<li><a href="#"><span class="font_awesome"></span></a></li><li>
+					<a class="btn_instagram" href="#"><span class="font_awesome"></span></a></li><li>
+					<a class="btn_pinterest" href="#"><span class="font_awesome"></span></a></li>
+				</ul>
 			</div>
-		</div>
 
-	</section>
+			<div class="mag_content_related group white_bg">
+				<div><img src="http://lorempixel.com/640/340/city" alt=""></div>
+				<div>
+					<h4 class="asap gray_medium">SIguiente Artículo:</h4>
+					<h3>10 Alimentos esenciales para estar Fit el dia de tu boda.</h3>
+				</div>
+			</div>
 
-	<?php include 'inc/sidebar.php' ?>
+		</section>
+
+		<?php include 'inc/sidebar.php' ?>
 
 	</div><!-- Group ends (Clearfix) -->
 
