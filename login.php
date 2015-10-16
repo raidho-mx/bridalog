4eaa0c567fd39e2186e584f75ff0cfe43e0e9214
Martinez <martinez@raidho.mx>
2015-10-16 18:28:14 -0500

    Imágenes en Proveedores actualizadas

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ Loging Template
 		<div class="login_welcome">
 			<p class="regular_title green">Bienvenido! Por favor ingresa tus datos para acceder a tu cuenta.</p>
 			<p class="asap">Edita tu perfil al publico, muestra tus nuevos productos, tus productos más notables y haz que te contacten en el catálogo más atractivo especializado en bodas.</p>
-			<img src="http://lorempixel.com/600/400/city" alt="">
+			<img src="lib/img/sample_imgs/login_welcome.png" alt="">
 		</div>
 		<div class="login_form_area white_bg asap">
 			<h4>Ya cuentas con tu membresía de Bridalog? Inicia sesión aquí.</h4>
--- proveedores.php
+++ proveedores.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Bridalog Website
-Index Template
+Template Proveedores
 */
 ?>
 
@@ -12,36 +12,51 @@ Index Template
 	<h1><strong>Proveedores Bridalog</strong></h1>
 
 	<!-- Main search results -->
-	<ul class="basic_grid three_col_grid">
+	<ul class="basic_grid three_col_grid prov_list_main">
 		<li><a href="#">
-				<img src="http://lorempixel.com/640/640/city" alt="">
-				<p class="asap gray_medium">Categoría</p>
-				<h4>Producto<span> por Proveedor</span></h4>
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/feat_elem_tres.jpg)"></div>
+				<p class="asap gray_medium">Mobiliario</p>
+				<h4>Elemento Tres</h4>
 			</a></li>
 		<li><a href="#">
-				<img src="http://lorempixel.com/640/640/city" alt="">
-				<p class="asap gray_medium">Categoría</p>
-				<h4>Producto<span> por Proveedor</span></h4>
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/eternity.jpg)"></div>
+				<p class="asap gray_medium">Joyas</p>
+				<h4>Eternity Diamonds</h4>
 			</a></li>
 		<li><a href="#">
-				<img src="http://lorempixel.com/640/640/city" alt="">
-				<p class="asap gray_medium">Categoría</p>
-				<h4>Producto<span> por Proveedor</span></h4>
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/sam.jpg)"></div>
+				<p class="asap gray_medium">Fotografía & Video</p>
+				<h4>Samuel Catherine</h4>
 			</a></li>
 		<li><a href="#">
-				<img src="http://lorempixel.com/640/640/city" alt="">
-				<p class="asap gray_medium">Categoría</p>
-				<h4>Producto<span> por Proveedor</span></h4>
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/mariana.jpg)"></div>
+				<p class="asap gray_medium">Wedding Planner</p>
+				<h4>Mariana Durán</h4>
 			</a></li>
 		<li><a href="#">
-				<img src="http://lorempixel.com/640/640/city" alt="">
-				<p class="asap gray_medium">Categoría</p>
-				<h4>Producto<span> por Proveedor</span></h4>
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/mona.jpg)"></div>
+				<p class="asap gray_medium">Vestidos & Accesorios</p>
+				<h4>Mona Mour Vestidos</h4>
 			</a></li>
 		<li><a href="#">
-				<img src="http://lorempixel.com/640/640/city" alt="">
-				<p class="asap gray_medium">Categoría</p>
-				<h4>Producto<span> por Proveedor</span></h4>
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/vinoteca.jpg)"></div>
+				<p class="asap gray_medium">Vinos</p>
+				<h4>Vinoteca</h4>
+			</a></li>
+		<li><a href="#">
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/liverpool.jpg)"></div>
+				<p class="asap gray_medium">Mesa de Regalos</p>
+				<h4>Liverpool valle Oriente</h4>
+			</a></li>
+		<li><a href="#">
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/feat_lunas_miel.jpg)"></div>
+				<p class="asap gray_medium">Luna de Miel</p>
+				<h4>Pérez Maldonado Agencia de Viajes</h4>
+			</a></li>
+		<li><a href="#">
+				<div style="background-image: url(lib/img/sample_imgs/01proveedores/bermellon.jpg)"></div>
+				<p class="asap gray_medium">Mesa de Postres</p>
+				<h4>Bermellón</h4>
 			</a></li>
 	</ul>
 
