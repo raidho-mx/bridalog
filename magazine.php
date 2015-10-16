63c8da19afd53075acce1d6e6a67ad1990f46067
Martinez <martinez@raidho.mx>
2015-10-16 18:49:21 -0500

    Include de Destacado en Catálogo incluído en Magazine

--- magazine.php
+++ magazine.php
@@ -179,27 +179,7 @@ Contact Template
 		</ul>
 	</div>
 
-	<!-- Destacados en el Catálogo -->
-	<div class="center_titles">
-		<h3 class="asap blue">Destacados en el Catálogo</h3>
-		<ul class="basic_grid three_col_grid">
-			<li><a href="#">
-					<img src="http://lorempixel.com/640/640/city" alt="">
-					<p class="asap gray_medium">Categoría</p>
-					<h4>Proveedor</span></h4>
-				</a></li>
-			<li><a href="#">
-					<img src="http://lorempixel.com/640/640/city" alt="">
-					<p class="asap gray_medium">Categoría</p>
-					<h4>Proveedor</span></h4>
-				</a></li>
-			<li><a href="#">
-					<img src="http://lorempixel.com/640/640/city" alt="">
-					<p class="asap gray_medium">Categoría</p>
-					<h4>Proveedor</span></h4>
-				</a></li>
-		</ul>
-	</div>
+	<?php include 'inc/feat_catalogo.php' ?>
 
 </div> <!--Wrap -->
 
