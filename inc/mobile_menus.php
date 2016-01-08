18719e7c4578b17f865dfb272574200a26dea3f1
pacster <martinez@raidho.mx>
2016-01-07 19:26:23 -0600

    Mejoras a menús de íconos
    - Se cambian PNG por SVG
    - Se pulen los tamaños específicos para teléfonos, tablets y pantallas grandes
    
    - Se pule la aparición de fondo blanco y bordes circulares dependiendo el contexto
    - Se mejora la apariencia de el listado de categorías en menú desplegable izquierdo

--- inc/mobile_menus.php
+++ inc/mobile_menus.php
@@ -25,28 +25,28 @@ Mobile Navigation Templates
 <!-- Your content -->
 	<nav role="navigation">
 		<ul class="asap">
-			<li><a href="#"><img src="lib/img/icons/icon-catering.png" alt=""><span href="" class="small_p">Catering</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-iglesia.png" alt=""><span href="" class="small_p">Coordinación Iglesia</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-decoracion.png" alt=""><span href="" class="small_p">Decoración</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-flores.png" alt=""><span href="" class="small_p">Flores</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-foto.png" alt=""><span href="" class="small_p">Fotografía & Video</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-iglesia-lugar.png" alt=""><span href="" class="small_p">Iglesias</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-iluminacion.png" alt=""><span href="" class="small_p">Iluminación</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-invitaciones.png" alt=""><span href="" class="small_p">Invitaciones & Detalles</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-joyas.png" alt=""><span href="" class="small_p">Joyas</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-lugar.png" alt=""><span href="" class="small_p">Lugar</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-luna.png" alt=""><span href="" class="small_p">Lunas de Miel</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-menswear.png" alt=""><span href="" class="small_p">Menswear</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-mesaregalos.png" alt=""><span href="" class="small_p">Mesa de Regalos</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-mesapostres.png" alt=""><span href="" class="small_p">Mesa de Postres</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-premaritales.png" alt=""><span href="" class="small_p">Pláticas Pre- Maritales</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-mobiliario.png" alt=""><span href="" class="small_p">Renta de Mobiliario</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-belleza.png" alt=""><span href="" class="small_p">Salón & Salud</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-otros.png" alt=""><span href="" class="small_p">Otros</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-vestidos.png" alt=""><span href="" class="small_p">Vestidos & Accesorios</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-vinos.png" alt=""><span href="" class="small_p">Vinos & Licores</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-planner.png" alt=""><span href="" class="small_p">Wedding Planner</span></a></li>
-			<li><a href="#"><img src="lib/img/icons/icon-zapatos.png" alt=""><span href="" class="small_p">Zapatos</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-catering.svg" alt=""><span href="" class="small_p">Catering</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-iglesia.svg" alt=""><span href="" class="small_p">Coordinación Iglesia</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-decoracion.svg" alt=""><span href="" class="small_p">Decoración</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-flores.svg" alt=""><span href="" class="small_p">Flores</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-foto.svg" alt=""><span href="" class="small_p">Fotografía & Video</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-iglesia-lugar.svg" alt=""><span href="" class="small_p">Iglesias</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-iluminacion.svg" alt=""><span href="" class="small_p">Iluminación</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-invitaciones.svg" alt=""><span href="" class="small_p">Invitaciones & Detalles</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-joyas.svg" alt=""><span href="" class="small_p">Joyas</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-lugar.svg" alt=""><span href="" class="small_p">Lugar</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-luna.svg" alt=""><span href="" class="small_p">Lunas de Miel</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-menswear.svg" alt=""><span href="" class="small_p">Menswear</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-mesaregalos.svg" alt=""><span href="" class="small_p">Mesa de Regalos</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-mesapostres.svg" alt=""><span href="" class="small_p">Mesa de Postres</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-premaritales.svg" alt=""><span href="" class="small_p">Pláticas Pre- Maritales</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-mobiliario.svg" alt=""><span href="" class="small_p">Renta de Mobiliario</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-belleza.svg" alt=""><span href="" class="small_p">Salón & Salud</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-otros.svg" alt=""><span href="" class="small_p">Otros</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-vestidos.svg" alt=""><span href="" class="small_p">Vestidos & Accesorios</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-vinos.svg" alt=""><span href="" class="small_p">Vinos & Licores</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-planner.svg" alt=""><span href="" class="small_p">Wedding Planner</span></a></li>
+			<li><a href="#"><img src="lib/img/icons/svg/icon-zapatos.svg" alt=""><span href="" class="small_p">Zapatos</span></a></li>
 		</ul>
 	</nav>
 </div>
